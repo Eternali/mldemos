20a249c426e38fa456a378a71cf9667c7a1f837b
Conrad Heidebrecht <conrad.heidebrecht@gmail.com>
2018-06-28 17:10:44 -0400

    working on home animation

--- lib/presentation/demo_button.dart
+++ lib/presentation/demo_button.dart
@@ -41,7 +41,7 @@ class DemoButtonState extends State<DemoButton> with SingleTickerProviderStateMi
   @override
   void initState() {
     super.initState();
-    _controller = AnimationController(duration: Duration(milliseconds: 1800), vsync: this)..repeat();
+    _controller = AnimationController(duration: Duration(milliseconds: 1900), vsync: this)..repeat();
   }
 
   @override
@@ -54,49 +54,51 @@ class DemoButtonState extends State<DemoButton> with SingleTickerProviderStateMi
   Widget build(BuildContext context) {
     final ThemeData theme = Theme.of(context);
 
-    return Stack(
-      children: <Widget>[
-        ScaleTransition(
-          scale: PulseTween(begin: 0.90, end: 0.96, delay: widget.animDelay + 0.12).animate(_controller),
-          child: Container(
-            margin: EdgeInsets.all(8.0),
-            alignment: Alignment.center,
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              color: (widget.color ?? theme.primaryColor).withOpacity(0.6),
-            ),
-          ),
-        ),
-        ScaleTransition(
-          scale: PulseTween(begin: 0.97, end: 1.0, delay: widget.animDelay).animate(_controller),
-          child: Container(
-            margin: EdgeInsets.all(32.0),
-            alignment: Alignment.center,
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              color: (widget.color ?? theme.primaryColor).withOpacity(0.6),
-              border: Border.all(
-                width: 2.0,
-                color: widget.color ?? theme.primaryColor
+    return Container(
+      child: Stack(
+        children: <Widget>[
+          ScaleTransition(
+            scale: PulseTween(begin: 0.93, end: 0.98, delay: widget.animDelay).animate(_controller),
+            child: Container(
+              margin: EdgeInsets.all(6.0),
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: (widget.color ?? theme.primaryColor).withOpacity(0.6),
               ),
             ),
           ),
-        ),
-        ScaleTransition(
-          scale: PulseTween(begin: 0.97, end: 1.02, delay: widget.animDelay + 0.06).animate(_controller),
-          child: Container(
-            margin: EdgeInsets.all(20.0),
-            alignment: Alignment.center,
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              color: (widget.color ?? theme.primaryColor).withOpacity(0.6),
-            ),
-            child: Text(
-              widget.title
+          ScaleTransition(
+            scale: PulseTween(begin: 1.0, end: 0.96, delay: widget.animDelay * 1.2).animate(_controller),
+            child: Container(
+              margin: EdgeInsets.all(42.0),
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: (widget.color ?? theme.primaryColor).withOpacity(0.6),
+                border: Border.all(
+                  width: 2.0,
+                  color: widget.color ?? theme.primaryColor
+                ),
+              ),
             ),
           ),
-        ),
-      ],
+          ScaleTransition(
+            scale: PulseTween(begin: 0.98, end: 1.02, delay: widget.animDelay * 1.4).animate(_controller),
+            child: Container(
+              margin: EdgeInsets.all(28.0),
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: (widget.color ?? theme.primaryColor).withOpacity(0.6),
+              ),
+              child: Text(
+                widget.title
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 
--- lib/presentation/home_screen.dart
+++ lib/presentation/home_screen.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 import 'package:mldemos/keys.dart';
 import 'package:mldemos/localizations.dart';
 import 'package:mldemos/containers/active_activity.dart';
 import 'package:mldemos/models/models.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
 
   final String title;
   final List<MenuChoice> overflow = [
@@ -24,49 +25,102 @@ class HomeScreen extends StatelessWidget {
   HomeScreen({ this.title }) : super(key: MLDemosKeys.homeScreen);
 
   @override
+  State<HomeScreen> createState() => HomeScreenState();
+
+}
+
+class HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateMixin {
+
+  AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(duration: Duration(seconds: 5), vsync: this)..repeat();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     final locales = MLDemosLocalizations.of(context);
     final ThemeData theme = Theme.of(context);
     final baseButtons = demos.values.map((demo) => demo.button).toList();
+    final buttonIdxs = List.generate(baseButtons.length, (b) => b);
 
     return ActiveActivity(
       builder: (BuildContext context, String activeRoute) {
         return Scaffold(
-          appBar: AppBar(
-            backgroundColor: Colors.transparent,
-            iconTheme: theme.iconTheme,
-            elevation: 0.0,
-            centerTitle: true,
-            title: Text(
-              title ?? locales.appTitle,
-              style: TextStyle(
-                color: theme.primaryColor,
+          body: Stack(
+            children: <Widget>[
+              Container(
+                decoration: BoxDecoration(
+                  gradient: new LinearGradient(
+                    begin: Alignment.topRight,
+                    end: Alignment.bottomLeft,
+                    stops: [0.1, 0.5, 0.7, 0.9],
+                    colors: [
+                      Colors.indigo[800],
+                      Colors.indigo[700],
+                      Colors.indigo[600],
+                      Colors.indigo[400],
+                    ],
+                  ),
+                ),
               ),
-            ),
-            actions: <Widget>[
-              PopupMenuButton<MenuChoice>(
-                icon: Icon(Icons.scatter_plot),
-                onSelected: (MenuChoice choice) {
-                  Navigator.of(context).pushNamed(choice.route);
-                },
-                itemBuilder: (BuildContext context) => overflow.map(
-                  (MenuChoice choice) => PopupMenuItem<MenuChoice>(
-                    value: choice,
-                    child: Row(
-                      children: <Widget>[
-                        Icon(choice.icon),
-                        Padding(padding: const EdgeInsets.symmetric(horizontal: 8.0)),
-                        Text(choice.title),
-                      ],
-                    ),
-                  )
-                ).toList(),
+              StaggeredGridView.count(
+                padding: EdgeInsets.only(top: 60.0),
+                primary: false,
+                crossAxisCount: 3,
+                children: buttonIdxs.map((b) => [
+                  [ baseButtons[b]..withDelay((b / buttonIdxs.length) + 0.2) ],
+                  b % 2 == 0 ? [ Container(), Container() ] : null,
+                ].where((b) => b != null)).expand((b) => b.expand((s) => s)).toList(),
+                staggeredTiles: buttonIdxs.map((b) {
+                  const spacers = [
+                    const StaggeredTile.count(1, 2),
+                    const StaggeredTile.count(2, 2)
+                  ];
+                  return b % 2 == 0 ? spacers.reversed : spacers;
+                }).expand((s) => s).toList(),
               ),
-            ],
-          ),
-          body: GridView.count(
-            crossAxisCount: 2,
-            children: baseButtons.map((btn) => btn..withDelay(baseButtons.indexOf(btn) / baseButtons.length)).toList(),
+              AppBar(
+                backgroundColor: Colors.transparent,
+                iconTheme: theme.iconTheme,
+                elevation: 0.0,
+                centerTitle: true,
+                title: Text(
+                  widget.title ?? locales.appTitle,
+                  style: TextStyle(
+                    color: theme.primaryColor,
+                  ),
+                ),
+                actions: <Widget>[
+                  PopupMenuButton<MenuChoice>(
+                    icon: Icon(Icons.scatter_plot),
+                    onSelected: (MenuChoice choice) {
+                      Navigator.of(context).pushNamed(choice.route);
+                    },
+                    itemBuilder: (BuildContext context) => widget.overflow.map(
+                      (MenuChoice choice) => PopupMenuItem<MenuChoice>(
+                        value: choice,
+                        child: Row(
+                          children: <Widget>[
+                            Icon(choice.icon),
+                            Padding(padding: const EdgeInsets.symmetric(horizontal: 8.0)),
+                            Text(choice.title),
+                          ],
+                        ),
+                      )
+                    ).toList(),
+                  ),
+                ],
+              ),
+            ]
           ),
         );
       }
